c458811b81af0369418d4b0768495c7fb3fd21e9
rainieyangcl <vutruonggiang2610@gmail.com>
2023-09-25 20:33:56 +0700

    EditUI

--- UI_projNCKH/Areas/Admin/Models/ForgotPasswordModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UI_projNCKH.Areas.Admin.Models
-{
-    public class ForgotPasswordModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
